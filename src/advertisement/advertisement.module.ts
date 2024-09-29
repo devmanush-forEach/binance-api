7400b9d8887db31f643cdd05100959e28d62d3f8
devmanush-forEach <shakyanikhil12345@gmail.com>
2024-09-29 22:34:37 +0530

    minor updates

--- src/advertisement/advertisement.module.ts
+++ src/advertisement/advertisement.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AdvertisementService } from './advertisement.service';
 import { AdvertisementController } from './advertisement.controller';
 import { Advertisement, AdvertisementSchema } from './advertisement.schema';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
+    forwardRef(() => AuthModule),
     MongooseModule.forFeature([
       { name: Advertisement.name, schema: AdvertisementSchema },
     ]),
@@ -13,4 +15,4 @@ import { Advertisement, AdvertisementSchema } from './advertisement.schema';
   controllers: [AdvertisementController],
   providers: [AdvertisementService],
 })
-export class AdvertisementModule {}
+export class AdvertisementModule { }
